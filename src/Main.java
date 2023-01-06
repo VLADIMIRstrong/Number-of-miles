b1bab260776967982077ca3568d640c6046ff056
Vladimir <vova,erohind82@mail.ru>
2023-01-06 10:25:32 +0300

    Gitignore added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 200;
+        int milePrice = 20;
+        int mile = ticketPrice / milePrice;
+        System.out.println(mile);
+        System.out.println("колличество миль начисленных за купленный билет");
+    }
+}
